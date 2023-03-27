8742468001c480cdf1c1a7441f89843e7dac0e0a
Kenshi23s <facucolombo@gmail.com>
2023-03-27 20:35:35 -0300

    attachment/hitscan comit

--- Assets/Guns/HitScan.cs
+++ Assets/Guns/HitScan.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using static Attachment;
 using static GunStats;
 
 public class HitScan
@@ -8,30 +9,54 @@ public class HitScan
     Camera cam;
     Transform myShootPos;
     float range;
+    GunFather myGun;
+    Transform _shootPos;
 
-    public HitScan(Camera cam, Transform shootPos, float range)
+
+    public HitScan(GunFather myGun)
     {
-        this.cam = cam;
-        myShootPos = shootPos;
-        this.range = range;
+        this.myGun = myGun;
     }
 
     //Action <RaycastHit> OnHit;
-    public bool HitSomething(out RaycastHit hit)
+    //public bool HitSomething(out RaycastHit hit)
+    //{
+    //    if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, stats.myGunStats[StatNames.Range]))
+    //    {
+    //        Vector3 dir = hit.point - myShootPos.position;
+    //        if (Physics.Raycast(myShootPos.transform.position, dir, out hit, range))
+    //        {
+               
+    //            return true;
+    //        }
+           
+    //    }
+    //    return false;
+    //}
+    public bool Shoot(BaseBulltet bullet)
     {
+
+        RaycastHit hit;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, stats.myGunStats[StatNames.Range]))
         {
-            Vector3 dir = hit.point - myShootPos.position;
+           
+            Vector3 dir = hit.point - _shootPos.position;
             if (Physics.Raycast(myShootPos.transform.position, dir, out hit, range))
             {
-               
+                // se llama a bullet manager para disparar la bala y se le pasa para donde debe ir
                 return true;
             }
-           
+
         }
         return false;
     }
 
+    void GetShootPos()
+    {       
+        //es bastante largo o esta bien?
+       _shootPos = myGun.myAttachMents.activeAttachments[AttachmentType.Muzzle].GetComponent<Muzzle>().shootPos;      
+    }
 
-    
+
+
 }
