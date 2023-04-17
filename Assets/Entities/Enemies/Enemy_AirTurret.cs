b6d135f5b614582e72b4ee5c4cf5a00346a19c6a
Kenshi23s <facucolombo@gmail.com>
2023-04-17 17:56:59 -0300

    Ielectronic

--- Assets/Entities/Enemies/Enemy_AirTurret.cs
+++ Assets/Entities/Enemies/Enemy_AirTurret.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_AirTurret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Transform pivotBaseRotation, pivotMisileRotation;
+    float _rotationX;
 
-    // Update is called once per frame
-    void Update()
+
+    Action align;
+    public void AlignCanon(Transform _target)
     {
-        
+        Vector3 dir = _target.position - transform.position;
+        Vector3 desiredForward = new Vector3(0,0, dir.z) - transform.forward;
+        pivotBaseRotation.forward += desiredForward.normalized * Time.deltaTime * _rotationX;
+        //if (Vector3.Distance(pivotBaseRotation.forward>))
+        //{
+
+        //}
     }
+    
 }
