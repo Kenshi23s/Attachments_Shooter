47f9c4bd9489edb8b2d28399e87435ed3cdccc37
Kenshi23s <facucolombo@gmail.com>
2023-04-22 22:28:29 -0300

    no pise algo

--- Assets/Scripts/Metodos Utiles/EasyRigidBody.cs
+++ Assets/Scripts/Metodos Utiles/EasyRigidBody.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public static class EasyRigidBody 
 {
+
    public static Rigidbody MoveTowards(this Rigidbody rb,Vector3 dir, float force)
    {
       rb.velocity = rb.velocity + dir.normalized * force * Time.deltaTime;
       return rb;
    }
 
-    public static Rigidbody ClampVelocity(this Rigidbody rb, float _maxSpeed)
-    {
-        rb.velocity= Vector3.ClampMagnitude(rb.velocity, _maxSpeed);
-        return rb;
-    }
+   public static Rigidbody ClampVelocity(this Rigidbody rb, float _maxSpeed)
+   {
+       rb.velocity= Vector3.ClampMagnitude(rb.velocity, _maxSpeed);
+       return rb;
+   }
 
    
 
