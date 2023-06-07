e06f0d0008034c186ac5917dcbc4bd2380e2f4dc
ErwinCurto <erwinnegocios@outlook.es>
2023-06-07 01:33:46 -0300

    tratando de hacer un shader para los yuyos altos (jocha te necestio)
    ayuda SOS

---
+++ Assets/#Scenes/0_Prefabs/Enviroment/planta estetica/CustomLighting.hlsl
@@ -0,0 +1,66 @@
+
+#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+#ifndef SHADERGRAPH_PREVIEW
+#include "Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/ShaderPass.hlsl"
+#if (SHADERPASS != SHADERPASS_FORWARD)
+#undef REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
+#endif
+#endif
+
+struct CustomLightingData{
+	
+	float3 positionWS;
+	float3 viewDirectionWS;
+	float4 shadowCoord;
+
+	float3 albedo;
+};
+
+#ifndef SHADERGRAPH_PREVIEW
+float3 CustomLightHandling(CustomLightingData d, Light light) {
+
+	float3 radiance = light.shadowAttenuation;
+	float3 color = d.albedo * radiance;
+
+	return color;
+}
+#endif
+
+float3 CalculateCustomLighting(CustomLightingData d) 
+{
+#ifdef SHADERGRAPH_PREVIEW
+	return d.albedo;
+#else
+	Light mainLight = GetMainLight(d.shadowCoord, d.positionWS, 1);
+
+	float3 color = 0;
+
+	color += CustomLightHandling(d, mainLight);
+
+	return color;
+#endif
+}
+
+void CalculateCustomLighting_float(float3 Position, float3 Normal, float3 ViewDirection, float3 Albedo, out float3 Color) 
+{
+	CustomLightingData d;
+	d.viewDirectionWS = ViewDirection;
+	d.albedo = Albedo;
+
+#ifdef SHADERGRAPH_PREVIEW
+	d.shadowCoord = 0;
+#else
+	float4 positionCS = TransformWorldToHClip(Position);
+#if SHADOWS_SCREEN
+	d.shadowCoord = ComputeScreenPos(positionCS);
+#else
+	d.shadowCoord = TransformWorldToShadowCoord(Position);
+#endif
+#endif
+
+	Color = CalculateCustomLighting(d);
+}
+
+#endif
