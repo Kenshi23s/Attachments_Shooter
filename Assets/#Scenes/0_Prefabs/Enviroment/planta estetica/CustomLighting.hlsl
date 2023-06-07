fd5fcc452ec9a642c61b213d0ef07811c7498c86
ErwinCurto <erwinnegocios@outlook.es>
2023-06-07 01:45:02 -0300

    arregle lo de soft shadows pero sigue tirando null referencie de "light" en shadows_shader

--- Assets/#Scenes/0_Prefabs/Enviroment/planta estetica/CustomLighting.hlsl
+++ Assets/#Scenes/0_Prefabs/Enviroment/planta estetica/CustomLighting.hlsl
@@ -3,10 +3,10 @@
 #define CUSTOM_LIGHTING_INCLUDED
 
 #ifndef SHADERGRAPH_PREVIEW
-#include "Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/ShaderPass.hlsl"
-#if (SHADERPASS != SHADERPASS_FORWARD)
-#undef REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
-#endif
+    #include "Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/ShaderPass.hlsl"
+    #if (SHADERPASS != SHADERPASS_FORWARD)
+        #undef REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
+    #endif
 #endif
 
 struct CustomLightingData{
