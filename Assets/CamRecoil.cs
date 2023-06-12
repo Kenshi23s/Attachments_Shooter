701f79e4626363d2db68914055c104df5dc5f392
Kenshi23s <facucolombo@gmail.com>
2023-06-12 17:52:14 -0300

    La muerte no nos concierne, pues, mientras existimos, la muerte no está presente y cuando llega la muerte nosotros ya no existimos

--- Assets/CamRecoil.cs
+++ Assets/CamRecoil.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static StatsHandler;
 
 public class CamRecoil : MonoBehaviour
 {
@@ -29,9 +30,9 @@ public class CamRecoil : MonoBehaviour
     void Update()
     {
         //la rotacion que quiero tener, siempre intenta volver a el medio
-        targetRotation=Vector3.Lerp(targetRotation,Vector3.zero,returnSpeed*Time.deltaTime);
+        targetRotation = Vector3.Lerp(targetRotation,Vector3.zero,returnSpeed*Time.deltaTime);
         //mi rotacion actual,intenta ir a mi rotacion target
-        currentRotation=Vector3.Slerp(currentRotation,targetRotation,snapines*Time.deltaTime);
+        currentRotation = Vector3.Slerp(currentRotation,targetRotation,snapines*Time.deltaTime);
         //seteo la rotacion actual
         transform.localRotation = Quaternion.Euler(currentRotation);
     }
@@ -39,7 +40,13 @@ public class CamRecoil : MonoBehaviour
     public void Recoil()
     {
         //se llama en onShoot, aumenta la rotacion(tendria que ser las stats del arma y no esto O la stats del arma + estos valores)
-        targetRotation += new Vector3(-VerticalRecoil, Random.Range(-HorizontalRecoil, HorizontalRecoil), 0);
+        float recoilY = Mathf.Min(-VerticalRecoil + _gunstats.GetStat(StatNames.VerticalRecoil), 0); 
+
+        float recoilx_minus = Mathf.Min(-HorizontalRecoil + _gunstats.GetStat(StatNames.HorizontalRecoil), 0); 
+      
+        float recoilX = Random.Range(recoilx_minus, recoilx_minus * -1);
+        Debug.Log(recoilY + " " + recoilX);
+        targetRotation += new Vector3(recoilY , recoilX, 0);
     }
 
     private void OnDrawGizmos()
