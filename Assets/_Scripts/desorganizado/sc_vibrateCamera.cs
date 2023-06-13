12e884f1cef59197283d2cc625838fb0eca5aa45
ErwinCurto <erwinnegocios@outlook.es>
2023-06-12 22:36:53 -0300

    cambio de script que vibra la camara

--- Assets/_Scripts/desorganizado/sc_vibrateCamera.cs
+++ Assets/_Scripts/desorganizado/sc_vibrateCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sc_ : MonoBehaviour
+public class sc_vibrateCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
