e762cec8191f13f4209196ed1e582a6c14379d08
josechaher <102690878+josechaher@users.noreply.github.com>
2023-06-17 18:44:40 -0300

    Scope Update

--- Assets/ScopeCamera.cs
+++ Assets/ScopeCamera.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 
 public class ScopeCamera : MonoBehaviour
 {
+    Sight _owner;
+    Transform _ogParent;
+    Vector3 _ogLocalPosition;
+    Quaternion _ogLocalRotation;
 
-
-   
-    private void Update()
+    private void Awake()
     {
-       
-        if (ShakeCamera.aiming)
-        {
+        _owner = GetComponentInParent<Sight>();
 
-            transform.position = Vector3.Lerp(transform.position, Camera.main.transform.position, Time.deltaTime);
-            transform.forward= Camera.main.transform.forward;
-        }
-        else
-        {
-            transform.position = Vector3.Lerp(transform.position, Vector3.zero, Time.deltaTime);
-        }
+        // Guardar valores originales
+        _ogParent = transform.parent;
+        _ogLocalPosition = transform.localPosition;
+        _ogLocalRotation = transform.localRotation;
+    }
+
+    private void Start()
+    {
+        _owner.onAttach += SetValuesToMainCamera;
+        _owner.onDettach += ResetValues;
+    }
+
+
+    void SetValuesToMainCamera() 
+    {
+        // Igualar a los valores de la camara principal
+        transform.parent = Camera.main.transform;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    void ResetValues() 
+    {
+        // Restaurar valores de la camara
+        transform.parent = _ogParent;
+        transform.localPosition = _ogLocalPosition;
+        transform.localRotation = _ogLocalRotation;
     }
 }
