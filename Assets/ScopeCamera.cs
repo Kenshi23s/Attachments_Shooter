7ee0553b715945861fc6c6c2ac2989d40fe1ab9f
josechaher <102690878+josechaher@users.noreply.github.com>
2023-06-18 12:50:08 -0300

    Rifle Scope Update + Attachment Bug Fix

--- Assets/ScopeCamera.cs
+++ Assets/ScopeCamera.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class ScopeCamera : MonoBehaviour
 {
     Sight _owner;
+    Camera _camera;
     Transform _ogParent;
     Vector3 _ogLocalPosition;
     Quaternion _ogLocalRotation;
 
+    [SerializeField]
+    float _additionalFOV = 5f;
+
+    [SerializeField]
+    Renderer _scopeRenderer;
+    Material _scopeMaterial;
+
+
+
     private void Awake()
     {
+        _camera = GetComponent<Camera>();
         _owner = GetComponentInParent<Sight>();
+        _scopeMaterial = _scopeRenderer.material;
+
+        TurnOffScope();
 
         // Guardar valores originales
         _ogParent = transform.parent;
@@ -32,6 +46,8 @@ public class ScopeCamera : MonoBehaviour
         transform.parent = Camera.main.transform;
         transform.localPosition = Vector3.zero;
         transform.localRotation = Quaternion.identity;
+
+        ShakeCamera.OnAimTransition += UpdateCamera;
     }
 
     void ResetValues() 
@@ -40,5 +56,29 @@ public class ScopeCamera : MonoBehaviour
         transform.parent = _ogParent;
         transform.localPosition = _ogLocalPosition;
         transform.localRotation = _ogLocalRotation;
+
+        TurnOffScope();
+        ShakeCamera.OnAimTransition -= UpdateCamera;
+    }
+
+    // Actualizar los valores de la mira y la camara segun que tan cerca esta de llegar a su posicion final de apuntado
+    void UpdateCamera(float t)
+    {
+        if (t > 0)
+        {
+            _camera.enabled = true;
+            _camera.fieldOfView = Camera.main.fieldOfView + _additionalFOV;
+            _scopeMaterial.SetFloat("_Fade", 1 - t);
+        }
+        else
+        {
+            TurnOffScope();
+        }
+    }
+
+    void TurnOffScope() 
+    {
+        _camera.enabled = false;
+        _scopeMaterial.SetFloat("_Fade", 1);
     }
 }
