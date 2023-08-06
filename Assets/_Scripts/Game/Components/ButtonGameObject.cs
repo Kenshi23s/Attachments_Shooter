4de10eb3c8991f33dfe4ef9be32e940736c45768
josechaher <102690878+josechaher@users.noreply.github.com>
2023-08-05 22:02:50 -0300

    Update ButtonGameObject.cs

--- Assets/_Scripts/Game/Components/ButtonGameObject.cs
+++ Assets/_Scripts/Game/Components/ButtonGameObject.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public interface IObjectSelectable : IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler, ISelectHandler, IDeselectHandler { }
+public interface IObjectSelectable : IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, ISelectHandler, IDeselectHandler { }
 /// <summary>
 /// Hace que un GameObject con collider actue como un botton. Se necesita un EventSystem en escena, y un PhysicsRaycaster acoplado a la camara.
 /// </summary>
@@ -13,17 +13,32 @@ public interface IObjectSelectable : IPointerEnterHandler, IPointerExitHandler, 
 [DisallowMultipleComponent]
 public class ButtonGameObject : MonoBehaviour, IObjectSelectable
 {
-    [field:SerializeField] public bool IsEnabled { get; private set; } = true;
-    public UnityEvent OnNormal, OnHighlighted, OnPressed, OnSelected;
-    public UnityEvent OnEnable, OnDisable;
+
+    bool _interactable = true;
+    public bool Interactable
+    {
+        get => _interactable;
+        set => SetInteractable(value);
+    }
+
+    bool _pointerIsHovering;
+    bool _pointerDownWhileInteractable;
+
+    public UnityEvent OnNormal = new UnityEvent();
+    public UnityEvent OnHighlighted = new UnityEvent();
+    public UnityEvent OnPressed = new UnityEvent();
+    public UnityEvent OnSelected = new UnityEvent();
+    public UnityEvent OnEnabled = new UnityEvent();
+    public UnityEvent OnDisabled = new UnityEvent();
 
     DebugableObject _debugger;
 
+
     public void ClearAllEvents()
     {
-        UnityEvent[] EventCol = 
+        UnityEvent[] EventCol =
         {
-             OnNormal,OnHighlighted, OnPressed, OnSelected ,OnEnable, OnDisable
+             OnNormal,OnHighlighted, OnPressed, OnSelected, OnEnabled, OnDisabled
         };
 
         //foreach (var item in EventCol.Where(x => x.GetPersistentEventCount() > 0).Where(x => x != null))
@@ -31,72 +46,115 @@ public class ButtonGameObject : MonoBehaviour, IObjectSelectable
         //    item.RemoveAllListeners();
         //}
     }
+
     private void Awake()
     {
+        Debug.Log("Awake called", gameObject);
+
         _debugger = GetComponent<DebugableObject>();
     }
 
     void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
     {
-        if (!IsEnabled || eventData.selectedObject == gameObject) return;
+        _pointerIsHovering = true;
+
+        if (!Interactable || eventData.selectedObject == gameObject) return;
 
         if (eventData.pointerPress)
         {
             OnPressed?.Invoke();
-            _debugger.Log("OBJECT STATE: PRESSED");
+            _debugger.Log("BUTTON STATE: PRESSED");
         }
         else
         {
             OnHighlighted?.Invoke();
-            _debugger.Log("OBJECT STATE: HIGHLIGHTED");
+            _debugger.Log("BUTTON STATE: HIGHLIGHTED");
         }
     }
 
     void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
     {
-        if (!IsEnabled || eventData.selectedObject == gameObject) return;
-
+        _pointerIsHovering = false;
+        if (!Interactable || eventData.selectedObject == gameObject) return;
+        
         OnNormal?.Invoke();
 
-        _debugger.Log("OBJECT STATE: NORMAL");
+        _debugger.Log("BUTTON STATE: NORMAL");
     }
 
     void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
     {
-        if (!IsEnabled || eventData.selectedObject == gameObject) return;
+        if (!_pointerDownWhileInteractable) return;
+
+        _pointerDownWhileInteractable = false;
+
+        if (!Interactable || eventData.selectedObject == gameObject) return;
 
         EventSystem.current.SetSelectedGameObject(gameObject, eventData);
     }
 
     void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
     {
-        if (!IsEnabled || eventData.selectedObject == gameObject) return;
+        if (!Interactable || eventData.selectedObject == gameObject) return;
 
+        _pointerDownWhileInteractable = true;
         OnPressed?.Invoke();
-        _debugger.Log("OBJECT STATE: PRESSED");
+        _debugger.Log("BUTTON STATE: PRESSED");
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        if (!_pointerIsHovering)
+            _pointerDownWhileInteractable = false;
     }
 
     void ISelectHandler.OnSelect(BaseEventData eventData)
     {
         OnSelected?.Invoke();
-        _debugger.Log("OBJECT STATE: SELECTED");
+        _debugger.Log("BUTTON STATE: SELECTED");
     }
 
     void IDeselectHandler.OnDeselect(BaseEventData eventData)
     {
         OnNormal?.Invoke();
-        _debugger.Log("OBJECT STATE: NORMAL");
+        _debugger.Log("BUTTON STATE: NORMAL");
     }
 
-    // Usar esta funcion para activar o desactivar el boton.
-    public void SetButtonState(bool enabled)
+    // Usar esta funcion para habilitar o deshabilitar el boton.
+    void SetInteractable(bool value)
     {
-        if (IsEnabled == enabled) return;
+        if (_interactable == value) return;
+        _interactable = value;
 
-        IsEnabled = enabled;
-        UnityEvent x = IsEnabled ? OnEnable : OnDisable;
-        x?.Invoke();
+        // Si se desactivo...
+        if (!_interactable)
+        {
+            OnDisabled?.Invoke();
 
-        _debugger.Log("OBJECT STATE: " + (IsEnabled ? "ENABLED" : "DISABLED"));
+            // Asegurarnos de que si se desactivo mientras se estaba presionando,
+            // no se tome el click cuando si se vuelve a activar.
+            _pointerDownWhileInteractable = false;
+
+            _debugger.Log("BUTTON STATE: DISABLED");
+            return;
+        }
+
+        // Si se activo...
+        OnEnabled?.Invoke();
+        _debugger.Log("BUTTON STATE: ENABLED");
+
+        // Chequear si el mouse esta por encima, y llamar los eventos indicados
+        if (_pointerIsHovering)
+        {
+            OnHighlighted?.Invoke();
+            _debugger.Log("BUTTON STATE: HIGHLIGHTED");
+        }
+        else
+        {
+            OnNormal?.Invoke();
+            _debugger.Log("BUTTON STATE: NORMAL");
+        }
     }
+
+
 }
