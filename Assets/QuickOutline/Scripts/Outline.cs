489db5dc5936b49157ab41483549069c7529d148
Kenshi23s <facucolombo@gmail.com>
2023-08-16 20:44:01 -0300

    Scene oops

--- Assets/QuickOutline/Scripts/Outline.cs
+++ Assets/QuickOutline/Scripts/Outline.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[DisallowMultipleComponent]
+[DisallowMultipleComponent,ExecuteInEditMode]
 
 public class Outline : MonoBehaviour
 {
