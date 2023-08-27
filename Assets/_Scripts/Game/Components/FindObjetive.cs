8a84ef164f52ccf6b1963f9a49c0045157221fe2
Kenshi23s <facucolombo@gmail.com>
2023-08-27 18:42:39 -0300

    Old Scan

--- Assets/_Scripts/Game/Components/FindObjetive.cs
+++ Assets/_Scripts/Game/Components/FindObjetive.cs
@@ -14,42 +14,12 @@ public class FindObjetive : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.P) && canTouchSpace) 
-        { 
+        if (Input.GetKeyDown(KeyCode.P) && canTouchSpace)
+        {
             StartCoroutine("TurnOnRadar");
             Debug.Log("Empezó la corrutina");
         }
     }
 
-    IEnumerator TurnOnRadar()
-    {
-        field.gameObject.SetActive(true);
-        canTouchSpace = false;
 
-        while(!inoffBig)
-        {
-            field.transform.localScale += Vector3.one*Time.deltaTime*increaseSpeed;
-            Debug.Log("Estoy aumentando la escala");
-
-            if (field.transform.localScale.x >= preferedFieldSize)
-            {
-                inoffBig = true;
-            }
-
-            yield return null;
-        }
-        
-        yield return new WaitForSeconds(5);
-        
-        field.transform.localScale = Vector3.one;
-        field.gameObject.SetActive(false);
-        Debug.Log("Volvio a la normalidad");
-
-        yield return new WaitForSeconds(waitingTime);
-
-        canTouchSpace = true;
-        inoffBig=false;
-        Debug.Log("Puedo volver a activar radar");
-
-    }
 }
