60b9f7b23127487ec77c3a26ca55e02fe03af74a
Kenshi23s <facucolombo@gmail.com>
2023-09-26 15:19:32 -0300

    editor Library removed

--- Assets/_Scripts/UI/UI Grabable Inventory/GrabableInventory.cs
+++ Assets/_Scripts/UI/UI Grabable Inventory/GrabableInventory.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static UI_GrabableItem;
-using static UnityEditor.Progress;
 
 public class GrabableInventory : MonoBehaviour
 {
